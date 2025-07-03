d253a65fe66ffb7a074a06d8f838617b54b04d20
Thomas Clark <tnc1997@outlook.com>
2025-07-03 23:32:43 +0100

    feat: add openid configuration routes and update token logic (#146)
    - Add joserfc package
    - Add issuer and key arguments
    - Add issuer and key configuration
    - Add import key function
    - Generate valid access tokens
    - Add openid configuration routes
    - Always add response headers
    - Fix broken tests
    - Add new test cases
    - Allow configuration with env variables
    - Update readme
    - Improve PR labeler workflow
    
    {major}
    
    ---------
    
    Signed-off-by: Thomas Clark <tnc1997@outlook.com>
    Signed-off-by: Esta Nagy <nagyesta@gmail.com>
    Co-authored-by: Esta Nagy <nagyesta@gmail.com>

--- src/python/app.py
+++ src/python/app.py
@@ -1,9 +1,16 @@
 import argparse
+import json
+import os
 import time
 from argparse import Namespace, ArgumentParser
 
+import flask
 from flask_parameter_validation import ValidateParameters, Query
-from flask import Flask
+from flask import Flask, request
+from joserfc import jwt
+from joserfc._keys import KeySetSerialization
+from joserfc.jwk import KeySet, RSAKey
+from joserfc.rfc7518.rsa_key import RSAKey
 
 app = Flask(__name__)
 
@@ -13,14 +20,43 @@ def get_token_metadata(resource: str) -> dict:
     epoch_time: int = int(time.time())
     expires_on: int = epoch_time + expires_in
 
+    header: dict = {
+        "alg": "RS256"
+    }
+
+    claims: dict = {
+        "iss": app.config.get("AID_ISSUER"),
+        "aud": resource,
+        "exp": expires_on,
+        "nbf": epoch_time,
+        "iat": epoch_time
+    }
+
+    token_value = jwt.encode(header, claims, app.config.get("AID_KEY"))
+
     return {
         "resource": resource,
-        "access_token": "dummy",
-        "refresh_token": "dummy",
+        "access_token": token_value,
+        "refresh_token": token_value,
         "expires_in": expires_in,
         "expires_on": expires_on,
         "token_type": "Bearer"
     }
+
+
+def init_key(key_path: str) -> RSAKey:
+    return generate_key() if key_path is None else import_key(key_path)
+
+
+def generate_key() -> RSAKey:
+    return RSAKey.generate_key(2048, {"use": "sig"})
+
+
+def import_key(path: str) -> RSAKey:
+    file = open(path, "rb")
+    key = RSAKey.import_key(file.read(), {"use": "sig"})
+    file.close()
+    return key
 
 
 url_regexp = ('^(http(s)?)://'
@@ -36,21 +72,55 @@ url_regexp = ('^(http(s)?)://'
               '(/[A-Za-z0-9.\\-_/]*)?$')
 
 
+def as_json_response(json_content: dict | KeySetSerialization) -> flask.Response:
+    resp = flask.Response(json.dumps(json_content))
+    resp.headers['Content-Type'] = 'application/json'
+    return resp
+
+
+@app.route(rule='/metadata/identity/.well-known/openid-configuration', methods=['GET'])
+@app.route(rule='/metadata/identity/.well-known/openid-configuration/', methods=['GET'])
+@ValidateParameters()
+def configuration():
+    return as_json_response({
+        "issuer": app.config.get("AID_ISSUER"),
+        "jwks_uri": f"{request.scheme}://{request.host}/metadata/identity/.well-known/openid-configuration/jwks"
+    })
+
+
+@app.route(rule='/metadata/identity/.well-known/openid-configuration/jwks', methods=['GET'])
+@app.route(rule='/metadata/identity/.well-known/openid-configuration/jwks/', methods=['GET'])
+@ValidateParameters()
+def jwks():
+    return as_json_response(KeySet([app.config.get("AID_KEY")]).as_dict())
+
+
 @app.route(rule='/metadata/identity/oauth2/token', methods=['GET'])
 @app.route(rule='/metadata/identity/oauth2/token/', methods=['GET'])
 @ValidateParameters()
 def token(resource: str = Query(pattern=url_regexp)):
-    return get_token_metadata(resource)
+    return as_json_response(get_token_metadata(resource))
 
 
 def process_arguments() -> Namespace:
     parser: ArgumentParser = argparse.ArgumentParser(description="Example script with arguments")
-    parser.add_argument("--host", help="The host name you want to use", type=str, default="0.0.0.0")
-    parser.add_argument("--port", help="The port number you want to use", type=int, default=5000)
+    parser.add_argument("--host", help="The host name you want to use", type=str,
+                        default="0.0.0.0")
+    parser.add_argument("--issuer", help="The issuer you want to use", type=str,
+                        default="https://sts.windows.net/00000000-0000-0000-0000-000000000000/")
+    parser.add_argument("--key", help="The key you want to use", type=str)
+    parser.add_argument("--port", help="The port number you want to use", type=int,
+                        default=5000)
     return parser.parse_args()
 
 
 if __name__ == '__main__':
     args: Namespace = process_arguments()
 
+    issuer = os.getenv("ASSUMED_ID_ISSUER") if os.getenv("ASSUMED_ID_ISSUER") else args.issuer
+    key_path = os.getenv("ASSUMED_ID_KEY_PATH") if os.getenv("ASSUMED_ID_KEY_PATH") else args.key
+    app.config.update({
+        "AID_ISSUER": issuer,
+        "AID_KEY": key_path
+    })
     app.run(args.host, args.port)
--- src/python/test.py
+++ src/python/test.py
@@ -4,19 +4,24 @@ import pytest
 
 import app
 
+
 @pytest.mark.skip(reason="helper method")
 def test_client():
     application = app.app
     application.config.update({
-        "TESTING": True,
+        "TESTING": True
     })
     return application.test_client()
 
 
 class AppTestCase(unittest.TestCase):
-    def test_json_should_contain_expected_Fields_when_called_with_valid_url(self):
+    def test_token_endpoint_json_should_contain_expected_Fields_when_called_with_valid_url(self):
         # given
         resource: str = "https://localhost:8443"
+        app.app.config.update({
+            "AID_ISSUER": "https://sts.windows.net/00000000-0000-0000-0000-000000000000",
+            "AID_KEY": app.generate_key()
+        })
 
         # when
         actual: dict = app.get_token_metadata(resource=resource)
@@ -44,9 +49,13 @@ class AppTestCase(unittest.TestCase):
         self.assertTrue(str(actual.json["error"]).startswith("Parameter 'resource' pattern does not match:"),
                         "Response should have been an error message")
 
-    def test_endpoint_should_return_token_when_valid_url_is_passed(self):
+    def test_token_endpoint_should_return_token_when_valid_url_is_passed(self):
         # given
         resource: str = "https://localhost:8443"
+        app.app.config.update({
+            "AID_ISSUER": "https://sts.windows.net/00000000-0000-0000-0000-000000000000",
+            "AID_KEY": app.generate_key()
+        })
 
         # when
         actual = test_client().get("/metadata/identity/oauth2/token?resource={}".format(resource))
@@ -54,6 +63,40 @@ class AppTestCase(unittest.TestCase):
         # then
         self.assertEqual(200, actual.status_code, "Response status should have been 200")
         self.assertEqual(resource, str(actual.json["resource"]), "Response should have the expected resource")
+
+    def test_configuration_endpoint_should_return_list_of_urls_when_called(self):
+        # given
+        issuer = "https://sts.windows.net/00000000-0000-0000-0000-000000000001"
+        app.app.config.update({
+            "AID_ISSUER": issuer,
+            "AID_KEY": app.generate_key()
+        })
+
+        # when
+        actual = test_client().get("/metadata/identity/.well-known/openid-configuration")
+
+        # then
+        self.assertEqual(200, actual.status_code, "Response status should have been 200")
+        self.assertEqual(issuer, str(actual.json["issuer"]), "Response should have the expected issuer")
+        self.assertTrue(
+            str(actual.json["jwks_uri"]).endswith("/metadata/identity/.well-known/openid-configuration/jwks"),
+            "Response should have the expected jwks_uri")
+
+    def test_jwks_endpoint_should_contain_key_details_when_called(self):
+        # given
+        app.app.config.update({
+            "AID_ISSUER": "https://sts.windows.net/00000000-0000-0000-0000-000000000000",
+            "AID_KEY": app.generate_key()
+        })
+
+        # when
+        actual = test_client().get("/metadata/identity/.well-known/openid-configuration/jwks")
+
+        # then
+        self.assertEqual(200, actual.status_code, "Response status should have been 200")
+        self.assertEqual(actual.json["keys"][0].keys(),
+                         {"n", "e", "d", "p", "q", "dp", "dq", "qi", "use", "kty", "kid"},
+                         "Response should have contained the key details")
 
 
 if __name__ == '__main__':
